da8b076dc26cab3ae89b98ab4e20c031d772b4ae
rogers <rogers@localhost>
2025-11-30 22:12:04 +0530

    w1e23r34tf

---
+++ 09Rav/src/test/java/ravan/ravan02.java
@@ -0,0 +1,40 @@
+package ravan;
+
+public class ravan02 {
+
+	public static void main(String[] args) {
+int a = 1;
+		
+		while(a<=23)
+		{
+			System.out.println(a);
+			a++;
+		}
+		
+		System.out.println();  //to change the line 
+		
+		// to print even numbers 
+		
+		int b = 2;
+		
+		while(b<=23)
+		{
+			System.out.println(b);
+			b+=2;
+		}
+		
+		System.out.println(); //to change the line 
+		
+		//to print decrement numbers
+		
+		int i = 20;
+		
+		while(i>0)
+		{
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
